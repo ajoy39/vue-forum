73d646edc82e5a7a6d323dfc87475feedb1c29c9
Alec Joy <alec@alecjoy.com>
2019-01-31 12:30:36 -0500

    organize posts:

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -1,37 +1,35 @@
 <template>
   <div class="col-large push-top">
     <h1>{{ thread.title }}</h1>
-    <div :key="postId" class="post" v-for="postId in thread.posts">
-      <div class="user-info">
-        <a href="#">{{ users[posts[postId].userId].name }}</a>
-        <a href="#">
-          <img class="avatar-large" :src="users[posts[postId].userId].avatar" alt="">
-        </a>
-      </div>
-      <div class="post-content">
-        <div>{{ posts[postId].text }}</div>
-      </div>
-      <div class="post-date text-faded">
-        {{ posts[postId].publishedAt }}
-      </div>
-    </div>
+    <PostList :posts="posts"/>
   </div>
 </template>
 <script>
   import sourceData from '@/data'
-
+  import PostList from '@/components/PostList'
   export default {
+    components: {
+      PostList
+    },
+
     props: {
       id: {
         required: true,
         type: String
       }
     },
+
     data () {
       return {
-        thread: sourceData.threads[this.id],
-        posts: sourceData.posts,
-        users: sourceData.users
+        thread: sourceData.threads[this.id]
+      }
+    },
+
+    computed: {
+      posts () {
+        const postIds = Object.values(this.thread.posts)
+        return Object.values(sourceData.posts)
+          .filter(post => postIds.includes(post['.key']))
       }
     }
   }
