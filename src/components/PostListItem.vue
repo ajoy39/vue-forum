eadb6140755b8f2f6c7297b13444ac390262ff4d
Alec Joy <alec@alecjoy.com>
2019-01-31 15:50:15 -0500

    move the date to a global component

--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -10,15 +10,14 @@
     <div class="post-content">
       <div>{{ post.text }}</div>
     </div>
-    <div class="post-date text-faded" :title="post.publishedAt | humanFriendlyDate">
-      {{ post.publishedAt | diffForHumans }}
+    <div class="post-date text-faded">
+      <AppDate :timestamp="post.publishedAt" />
     </div>
   </div>
 </template>
 
 <script>
   import sourceData from '@/data'
-  import moment from 'moment'
 
   export default {
     props: {
@@ -35,16 +34,6 @@
 
       userPostsCount () {
         return Object.keys(this.user.posts).length
-      }
-    },
-
-    filters: {
-      humanFriendlyDate (date) {
-        return moment.unix(date).format('MMMM Do YYYY h:mm a')
-      },
-
-      diffForHumans (date) {
-        return moment.unix(date).fromNow()
       }
     }
   }
