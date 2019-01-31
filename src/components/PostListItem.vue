07a7326634020ab81105149401b1296a47c70554
Alec Joy <alec@alecjoy.com>
2019-01-31 15:30:09 -0500

    add human readable dates and diff in dates to forum posts using moment.js

--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -10,14 +10,15 @@
     <div class="post-content">
       <div>{{ post.text }}</div>
     </div>
-    <div class="post-date text-faded">
-      {{ post.publishedAt }}
+    <div class="post-date text-faded" :title="post.publishedAt | humanFriendlyDate">
+      {{ post.publishedAt | diffForHumans }}
     </div>
   </div>
 </template>
 
 <script>
   import sourceData from '@/data'
+  import moment from 'moment'
 
   export default {
     props: {
@@ -34,6 +35,16 @@
 
       userPostsCount () {
         return Object.keys(this.user.posts).length
+      }
+    },
+
+    filters: {
+      humanFriendlyDate (date) {
+        return moment.unix(date).format('MMMM Do YYYY h:mm a')
+      },
+
+      diffForHumans (date) {
+        return moment.unix(date).fromNow()
       }
     }
   }
