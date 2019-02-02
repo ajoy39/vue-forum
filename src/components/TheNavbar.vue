57af556db2276a852fef9e78f78ce68cbf8f4690
Alec Joy <alec@alecjoy.com>
2019-02-02 13:18:30 -0500

    add a navbar

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <header class="header" id="header">
+        <router-link :to="{name: 'Home'}" class="logo">
+          <img src="../assets/img/vueschool-logo.svg">
+        </router-link>
+
+        <div class="btn-hamburger">
+            <!-- use .btn-humburger-active to open the menu -->
+            <div class="top bar"></div>
+            <div class="middle bar"></div>
+            <div class="bottom bar"></div>
+        </div>
+
+    <!-- use .navbar-open to open nav -->
+    <nav class="navbar">
+        <ul>
+            <li class="navbar-user">
+                <a href="#">
+                    <img class="avatar-small" src="https://pbs.twimg.com/profile_images/881260299420041217/GMVGlDea_400x400.jpg" alt="">
+                    <span>
+                        Alex Kyriakidis
+                        <img class="icon-profile" src="../assets/img/arrow-profile.svg" alt="">
+                    </span>
+                </a>
+
+                <!-- dropdown menu -->
+                <!-- add class "active-drop" to show the dropdown -->
+                <div id="user-dropdown">
+                    <div class="triangle-drop"></div>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown-menu-item"><a href="profile.html">View profile</a></li>
+                        <li class="dropdown-menu-item"><a href="#">Log out</a></li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </nav>
+  </header>
+</template>
